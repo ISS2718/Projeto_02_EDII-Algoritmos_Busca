51789a30d22845d716109d18c7de2458373a6d57
Isaac Soares <isaacsantos.soares@hotmail.com>
2022-07-21 20:23:29 -0300

    Update busca_2b.c

--- busca_2b.c
+++ busca_2b.c
@@ -70,8 +70,55 @@ unsigned h_duplo (unsigned x, unsigned i, unsigned B) {
    return (h_mul(x, i, B) + i * h_div(x, i, B))%B;
 }
 
-int insere_hash (string* hash, string s, unsigned B) {
+void criar(string ** t, int B){
+    *t = malloc(B*sizeof(string));
+    for(int i = 0; i<B; i++){
+        (*t)[i] = malloc(25*sizeof(char));
+        strcpy((*t)[i], "-1");
+    }
+}
 
+void destruir(string ** t, int B){
+  for(int i = 0; i<B; i++){
+    free((*t)[i]);
+  }
+  free(*t);
+}
+
+int inserir_duplo(string ** t, unsigned B, string k) {
+    int colisoes = 0;
+    for(int i=0; i<B; i++){
+        int x = converter(k);
+        int pos = h_duplo(x, i, B);
+        if(strcmp((*t)[pos], "-1") == 0){// posicao nunca ocupada
+            strcpy((*t)[pos], k);
+            //printf("Armazenou %s\n", (*t)[pos]);
+            return colisoes;
+        }
+        if(strcmp((*t)[pos], k) == 0 ){
+            //printf("Palavra %s ja cadastrada\n", (*t)[pos]);
+            return colisoes;
+        }
+        //Se chegou aqui a posicao nao estava vazia e palavra nao era repetida => houve colisao
+        colisoes++;
+    }
+    return colisoes;
+}
+
+int buscar_duplo(string * t, unsigned B, string k){
+    for(int i=0; i<B; i++){
+        int x = converter(k);
+        int pos = h_duplo(x, i, B);
+        if(strcmp(t[pos], k) == 0 ){
+            //printf("Palavra %s encontrada na posicao %d\n", t[pos], pos);
+            return 0;
+        }
+        if(strcmp(t[pos], "-1") == 0){//sao iguais -> posicao nunca ocupada ou palavra repetida
+            //printf("Palavra %s nao existe na lista\n", k);
+            return -1;
+        }
+    }
+    return -1;
 }
 
 int main(int argc, char const *argv[])
@@ -83,29 +130,44 @@ int main(int argc, char const *argv[])
     unsigned colisoes = 0;
     unsigned encontrados = 0;
 
-    string* insercoes = ler_strings("strings_entrada.txt", N);
-    string* consultas = ler_strings("strings_busca.txt", M);
-
+    string* insercoes = ler_strings("dado/strings_entrada.txt", N);
+    string* consultas = ler_strings("dado/strings_busca.txt", M);
 
     // cria tabela hash com hash por hash duplo
-    string* hash = malloc(sizeof(string)*B);
+    string *t;
+    criar(&t, B);
     // inserção dos dados na tabela hash
     inicia_tempo();
     for (int i = 0; i < N; i++) {
         // inserir insercoes[i] na tabela hash
+        colisoes += inserir_duplo(&t, B, insercoes[i]);
     }
     double tempo_insercao = finaliza_tempo();
-
+    
+    for (int i = 0; i < N; i++) {
+        free(insercoes[i]);
+    }
+    free(insercoes);
+  
     // busca dos dados na tabela hash
     inicia_tempo();
     for (int i = 0; i < M; i++) {
         // buscar consultas[i] na tabela hash
+        if(buscar_duplo(t, B, consultas[i]) == 0){
+            encontrados++;
+        }
     }
     double tempo_busca = finaliza_tempo();
 
+    for (int i = 0; i < M; i++) {
+        free(consultas[i]);
+    }
+    free(consultas);
 
-    printf("Colisões na inserção: %d\n", colisoes);
-    printf("Tempo de inserção   : %fs\n", tempo_insercao);
+    destruir(&t, B);
+    
+    printf("Colisoes na insercao: %d\n", colisoes);
+    printf("Tempo de insercao   : %fs\n", tempo_insercao);
     printf("Tempo de busca      : %fs\n", tempo_busca);
     printf("Itens encontrados   : %d\n", encontrados);
 
